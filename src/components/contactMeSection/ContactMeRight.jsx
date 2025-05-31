2e2cdaa029bcb9917878826154572a777ea606a0
unknown <you@example.com>
2025-05-31 23:37:12 +0530

    change

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -1,13 +1,13 @@
 import ContactInfo from "./ContactInfo";
 import ContactSocial from "./ContactSocial";
-import emailImage from "../../assets/images/email-image.png";
+import emailImage from "../../assets/images/emailimage.png";
 
 const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
       <img
-        src={emailImage}
-        alt="email image"
+        src= {emailImage}
+        alt="Email Image"
         className="max-w-[300px]"
       />
       <ContactInfo />
